f43c47eac2fa5c910e83c78998b44d9888c896e7
FreeFlight-Code <davidfisc@hotmail.com>
2017-10-26 22:34:20 -0600

    trying to add combineReducers, app works but not with combine

--- src/store.js
+++ src/store.js
@@ -1,5 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
-import reducer from './ducks/user';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import UserReducer from './ducks/user';
+import AccountReducer from './ducks/account';
 import promiseMiddleware from 'redux-promise-middleware';
 
-export default createStore(reducer, {}, applyMiddleware(promiseMiddleware()));
+export default createStore(UserReducer, {}, applyMiddleware(promiseMiddleware()));
+
+
+// const reducers= {
+//     user: UserReducer,
+//     account: AccountReducer
+
+// }
+// const reducer = combineReducers(reducers);
+
+// export default createStore(reducer, {}, applyMiddleware(promiseMiddleware()));
